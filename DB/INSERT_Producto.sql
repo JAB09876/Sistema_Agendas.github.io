19e16a803feed344d14dcba62c42b3f37e8b36ce
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-07 18:56:07 -0600

    Update INSERT_Producto.sql

--- DB/INSERT_Producto.sql
+++ DB/INSERT_Producto.sql
@@ -1,3 +1,6 @@
+ALTER TABLE Producto
+MODIFY COLUMN imagen LONGBLOB NULL; 
+
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Crema Hidratante para Bebés', 'Hidratación suave y protectora para la piel delicada del bebé', 10.00, 1, 1, 1);
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Fórmula Infantil en Polvo', 'Fórmula nutricionalmente completa para lactantes y niños pequeños', 20.00, 1, 2, 2);
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Set de Bloques de Construcción', 'Juguete educativo para fomentar la creatividad y habilidades motoras', 15.00, 1, 3, 3);
@@ -8,3 +11,6 @@ INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca)
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Vestido de Algodón para Niñas', 'Ropa elegante y cómoda para niñas en algodón suave', 30.00, 1, 4, 2);
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Toalla de Baño con Capucha', 'Toalla absorbente y suave con capucha para bebés después del baño', 14.00, 1, 5, 1);
 INSERT INTO Producto (nombre, descripcion, precio, estado, idCategoria, idMarca) VALUES ('Set de Vajilla Infantil', 'Vajilla colorida y segura para niños pequeños con diseños divertidos', 20.00, 1, 3, 3);
+
+ALTER TABLE Producto
+MODIFY COLUMN imagen LONGBLOB NOT NULL; 
