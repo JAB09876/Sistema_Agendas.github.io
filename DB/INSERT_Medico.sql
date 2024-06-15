9748fa70632a6a24b432beca2857a860aee9c56b
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-15 11:22:47 -0600

    Create INSERT_Medico.sql

---
+++ DB/INSERT_Medico.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Juan Pérez', 1, 1, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. María López', 1, 2, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Carlos Sánchez', 1, 3, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Ana González', 1, 4, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Pedro Rodríguez', 1, 5, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Laura Fernández', 1, 6, 1);
+INSERT INTO `Sistema_Agenda`.`Medico` (nombre, estado, idEspecilidad, idSucursal) VALUES ('Dr. Jorge Martínez', 1, 7, 1);
