312c63a8f54c79bdbd87332dace407f551d27450
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-14 19:07:48 -0600

    Delete DB/Categoria.sql

--- DB/Categoria.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Categoria (descripcion) VALUES ('Cremas y Lociones para Bebés');
-INSERT INTO Categoria (descripcion) VALUES ('Suplementos Nutricionales Infantiles');
-INSERT INTO Categoria (descripcion) VALUES ('Juguetes Educativos');
-INSERT INTO Categoria (descripcion) VALUES ('Ropa Infantil');
-INSERT INTO Categoria (descripcion) VALUES ('Accesorios para el Baño');
