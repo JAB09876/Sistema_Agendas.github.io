1ea742b562fc1ea600433bb95bd3898061dc053c
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-14 19:08:14 -0600

    Create INSERT_Categoria.sql

---
+++ DB/INSERT_Categoria.sql
@@ -0,0 +1,5 @@
+INSERT INTO Categoria (descripcion) VALUES ('Cremas y Lociones para Bebés');
+INSERT INTO Categoria (descripcion) VALUES ('Suplementos Nutricionales Infantiles');
+INSERT INTO Categoria (descripcion) VALUES ('Juguetes Educativos');
+INSERT INTO Categoria (descripcion) VALUES ('Ropa Infantil');
+INSERT INTO Categoria (descripcion) VALUES ('Accesorios para el Baño');
