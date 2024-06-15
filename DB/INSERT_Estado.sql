03a42fccc90ed05a35038112f3dd5435fd33a96e
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-15 11:29:03 -0600

    Create INSERT_Estado.sql

---
+++ DB/INSERT_Estado.sql
@@ -0,0 +1,6 @@
+INSERT INTO Estado (id, descripcion) VALUES (1, 'Pendiente');
+INSERT INTO Estado (id, descripcion) VALUES (2, 'Confirmada');
+INSERT INTO Estado (id, descripcion) VALUES (3, 'Reprogramada');
+INSERT INTO Estado (id, descripcion) VALUES (4, 'Completada');
+INSERT INTO Estado (id, descripcion) VALUES (5, 'Cancelada');
+INSERT INTO Estado (id, descripcion) VALUES (6, 'No asistió');
