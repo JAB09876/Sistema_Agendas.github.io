7c9dd3bd19bf8b98d33ec7504784f086204e5bd3
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-15 11:37:44 -0600

    Create INSERT_Cita.sql

---
+++ DB/INSERT_Cita.sql
@@ -0,0 +1,17 @@
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('159357852', '2024-06-20 09:00:00', 900, 1000, 1, 2, 1);
+
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('369147258', '2024-06-20 10:00:00', 1000, 1100, 1, 2, 1);
+
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('369258147', '2024-06-21 11:00:00', 1100, 1200, 1, 3, 1);
+
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('741852963', '2024-06-21 12:00:00', 1200, 1300, 1, 4, 1);
+
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('789456123', '2024-06-22 13:00:00', 1300, 1400, 1, 5, 1);
+
+INSERT INTO `Sistema_Agenda`.`Cita` (idCliente, fecha, horaInicio, horaFin, idEstado, idMedico, idSucursal) 
+VALUES ('987654321', '2024-06-22 13:00:00', 1300, 1400, 1, 4, 1);
