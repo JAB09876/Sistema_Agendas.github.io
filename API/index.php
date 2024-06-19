22705a1e0b58fcb27dac9747e43269d042a47a92
Daniela Méndez <140221376+danielamendezp@users.noreply.github.com>
2024-06-18 21:01:37 -0600

    Add files via upload

--- API/index.php
+++ API/index.php
@@ -16,25 +16,23 @@ require_once "models/UsuarioModel.php";
 require_once "models/LoginModel.php";
 require_once "models/CategoriaModel.php";
 require_once "models/ProductoModel.php";
-require_once "models/MarcaModel.php";
+require_once "models/CitaModel.php";
 require_once "models/EspecialidadModel.php";
+require_once "models/MedicoModel.php";
 require_once "models/ServicioModel.php";
 require_once "models/SubServicioModel.php";
-require_once "models/MedicoModel.php";
-require_once "models/CitaModel.php";
-require_once "models/FacturaModel.php";
+require_once "models/SucursalModel.php";
 /***--- Agregar todos los controladores*/
 require_once "controllers/UsuarioController.php";
 require_once "controllers/LoginController.php";
 require_once "controllers/CategoriaController.php";
-require_once "controllers/ProductoContoller.php";
-require_once "controllers/MarcaController.php";
+require_once "controllers/ProductoController.php";
+require_once "controllers/CitaController.php";
 require_once "controllers/EspecialidadController.php";
+require_once "controllers/MedicoController.php";
 require_once "controllers/ServicioController.php";
 require_once "controllers/SubServicioController.php";
-require_once "controllers/MedicoController.php";
-require_once "controllers/CitaController.php";
-require_once "controllers/FacturaController.php";
+require_once "controllers/SucursalController.php";
 //Enrutador
 //RoutesController.php
 require_once "controllers/RoutesController.php";
