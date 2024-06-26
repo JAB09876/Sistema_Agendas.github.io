018729caf86794caeb51fdb07830f70881d13495
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-25 18:42:20 -0600

    Update INSERT_FacturaDetalle.sql

--- DB/INSERT_FacturaDetalle.sql
+++ DB/INSERT_FacturaDetalle.sql
@@ -12,18 +12,3 @@ VALUES (4, 250.00, 1, NULL, 4, 1);
 
 INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
 VALUES (5, 300.00, 1, NULL, 5, 1);
-
-INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
-VALUES (6, 350.00, 1, NULL, 6, 1);
-
-INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
-VALUES (7, 400.00, 1, NULL, 7, 1);
-
-INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
-VALUES (8, 450.00, 1, NULL, 8, 1);
-
-INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
-VALUES (9, 500.00, 1, NULL, 9, 1);
-
-INSERT INTO `Sistema_Agenda`.`Factura_Detalle` (idFactura, subtotal, estado, idProducto, idSubservicio, cantidad) 
-VALUES (10, 550.00, 1, NULL, 10, 1);
