64bb4778ccb17a1cf730a1c6cfb4912452cfbf18
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-28 23:14:46 -0600

    Update INSERT_Sucursal.sql

--- DB/INSERT_Sucursal.sql
+++ DB/INSERT_Sucursal.sql
@@ -1,6 +1,11 @@
 INSERT INTO `Sistema_Agenda`.`Sucursal` 
 (`nombre`, `telefono`, `estado`, `correo_electronico`, `direccion_exacta`) 
 VALUES 
+('Sucursal San José', '22223333', 1, 'sanjose@sistema_agenda.cr', 'Avenida Central, San José, Costa Rica');
+
+INSERT INTO `Sistema_Agenda`.`Sucursal` 
+(`nombre`, `telefono`, `estado`, `correo_electronico`, `direccion_exacta`) 
+VALUES 
 ('Sucursal Alajuela', '24445555', 1, 'alajuela@sistema_agenda.cr', 'Calle Central, Alajuela, Costa Rica');
 
 INSERT INTO `Sistema_Agenda`.`Sucursal` 
