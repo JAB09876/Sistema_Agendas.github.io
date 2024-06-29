5a0728db30e28a7951e99dbe64efb1b1c8ae2149
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-06-28 22:55:18 -0600

    Update INSERT_Sucursal.sql

--- DB/INSERT_Sucursal.sql
+++ DB/INSERT_Sucursal.sql
@@ -1,14 +1,9 @@
 INSERT INTO `Sistema_Agenda`.`Sucursal` 
-(`nombre`, `telefono`, `correo_electronico`, `direccion_exacta`) 
+(`nombre`, `telefono`, `estado`, `correo_electronico`, `direccion_exacta`) 
 VALUES 
-('Sucursal San José', '22223333', 'sanjose@sistema_agenda.cr', 'Avenida Central, San José, Costa Rica');
+('Sucursal Alajuela', '24445555', 1, 'alajuela@sistema_agenda.cr', 'Calle Central, Alajuela, Costa Rica');
 
 INSERT INTO `Sistema_Agenda`.`Sucursal` 
-(`nombre`, `telefono`, `correo_electronico`, `direccion_exacta`) 
+(`nombre`, `telefono`, `estado`, `correo_electronico`, `direccion_exacta`) 
 VALUES 
-('Sucursal Alajuela', '24445555', 'alajuela@sistema_agenda.cr', 'Calle Central, Alajuela, Costa Rica');
-
-INSERT INTO `Sistema_Agenda`.`Sucursal` 
-(`nombre`, `telefono`, `correo_electronico`, `direccion_exacta`) 
-VALUES 
-('Sucursal Heredia', '22667788', 'heredia@sistema_agenda.cr', 'Boulevard de las Flores, Heredia, Costa Rica');
+('Sucursal Heredia', '22667788', 1, 'heredia@sistema_agenda.cr', 'Boulevard de las Flores, Heredia, Costa Rica');
