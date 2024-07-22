b3991efd16a6d560378be393dee6405136ed7708
JAB09876 <117924761+JAB09876@users.noreply.github.com>
2024-07-22 16:24:54 -0600

    Delete DB directory

--- DB/INSERT_Estado.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Estado (id, descripcion) VALUES (1, 'Pendiente');
-INSERT INTO Estado (id, descripcion) VALUES (2, 'Confirmada');
-INSERT INTO Estado (id, descripcion) VALUES (3, 'Reprogramada');
-INSERT INTO Estado (id, descripcion) VALUES (4, 'Completada');
-INSERT INTO Estado (id, descripcion) VALUES (5, 'Cancelada');
-INSERT INTO Estado (id, descripcion) VALUES (6, 'No asistió');
--- DB/INSERT_Marca.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Marca (nombre) VALUES ('BabyGlow');
-INSERT INTO Marca (nombre) VALUES ('TinyTummies');
-INSERT INTO Marca (nombre) VALUES ('SmartToys');
